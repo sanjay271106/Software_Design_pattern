bb52b3b68e6ba52b8796f3671298c6081e793c2d
SANJAY <sanjayraghuraman11@gmail.com>
2024-07-31 09:44:35 +0530

    Update Rent.jsx

--- sanjay/src/pages/shared/Rent.jsx
+++ sanjay/src/pages/shared/Rent.jsx
@@ -48,7 +48,7 @@ export const Rent = () => {
               </ul>
                 <button className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">Schedule a Tour</button>
                 <button className="bg-orange-600 text-white px-6 py-2 rounded-lg hover:bg-orange-700 transition ml-[2rem]">Contact Owner</button>
-                <button className="bg-black text-white px-6 py-2 rounded-lg hover:bg-gray-700 transition ml-[2rem]">Contact Owner</button>
+                <button className="bg-black text-white px-6 py-2 rounded-lg hover:bg-gray-700 transition ml-[2rem]">Add Favorites</button>
             </div>
 
             {/* Description and Features */}
